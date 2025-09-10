eac9b7142749cf5139881b2a9b58e7369a539fae
Vyom1111 <118009657+Vyom1111@users.noreply.github.com>
2025-09-10 23:12:22 +0530

    Delete source/api directory

--- source/api/index.mjs
+++
@@ -1,9 +0,0 @@
-// source/api/index.mjs
-import app from "../app.mjs";
-
-// Export a handler that Vercel can run.
-// This forwards every request to your Express app.
-export default function handler(req, res) {
-  return app(req, res);
-}
-
